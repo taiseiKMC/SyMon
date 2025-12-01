9f5d7dc2a955753a32899f3f5a22d603ce73c6d1
taiseiKMC <taisei@kmc.gr.jp>
2025-12-01 22:30:15 +0900

    Add operator<< for vector<T>
    With removing the same function from automaton_parser.hh

--- src/automaton_parser.hh
+++ src/automaton_parser.hh
@@ -76,20 +76,6 @@ static inline std::ostream &operator<<(std::ostream &os, const TimingConstraint<
   return os;
 }
 
-template <class T> static inline std::ostream &operator<<(std::ostream &os, const std::vector<T> &guard) {
-  bool first = true;
-  os << "{";
-  for (const auto &g: guard) {
-    if (!first) {
-      os << ", ";
-    }
-    first = false;
-    os << g;
-  }
-  os << "}";
-  return os;
-}
-
 template <typename Timestamp>
 static inline std::istream &operator>>(std::istream &is, TimingConstraint<Timestamp> &p) {
   if (is.get() != 'x') {
